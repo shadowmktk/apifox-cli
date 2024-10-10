4c6f29485e49601ff80ab1635f78521cad2b1e87
shadowmktk <71061903+shadowmktk@users.noreply.github.com>
2024-10-10 09:47:47 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 apifox run \
   --access-token "${APIFOX_ACCESS_TOKEN}" \
-  -n "${APIFOX_ITERATION_COUNT}" \
+  -n "${APIFOX_ITERATION}" \
   -r "${APIFOX_REPORTERS}" \
   -t "${APIFOX_SCENARIO_ID}" \
   -e "${APIFOX_ENVIRONMENT}" 
